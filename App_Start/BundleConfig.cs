5443f74b8bc23090e98afaabe8bc61d79698b139
granthalsey <granthalsey@gmail.com>
2017-07-19 15:27:29 -0400

    a

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -5,48 +5,48 @@ using System.Web.Optimization;
 
 namespace MvcApplication1
 {
-    public class PartialsTransform : IBundleTransform
-    {
-        private readonly string _moduleName;
-        private readonly string _physicalPath;
-        public PartialsTransform(string moduleName)
-        {
-            _moduleName = moduleName;
-        }
+    //public class PartialsTransform : IBundleTransform
+    //{
+    //    private readonly string _moduleName;
+    //    private readonly string _physicalPath;
+    //    public PartialsTransform(string moduleName)
+    //    {
+    //        _moduleName = moduleName;
+    //    }
 
-        public void Process(BundleContext context, BundleResponse response)
-        {
-            var strBundleResponse = new StringBuilder();
-            // Javascript module for Angular that uses templateCache 
-            strBundleResponse.AppendFormat(
-                @"angular.module('{0}').run(['$templateCache',function(t){{",
-                _moduleName);
+    //    public void Process(BundleContext context, BundleResponse response)
+    //    {
+    //        var strBundleResponse = new StringBuilder();
+    //        // Javascript module for Angular that uses templateCache 
+    //        strBundleResponse.AppendFormat(
+    //            @"angular.module('{0}').run(['$templateCache',function(t){{",
+    //            _moduleName);
 
-            foreach (var file in response.Files)
-            {
-                // Get content of file
-                var content = file.ApplyTransforms();
-                // Remove newlines and replace ' with \\'
-                content = content.Replace("'", "\\'").Replace("\r\n", "");
-                // Find templateUrl by getting file path and removing inital ~
-                var templateUrl = file.IncludedVirtualPath.Replace("~", "").Replace('\\', '/');
-                // Add content of template file inside an Angular put method
-                strBundleResponse.AppendFormat("t.put('{0}','{1}');", templateUrl, content);
-            }
-            strBundleResponse.Append(@"}]);");
+    //        foreach (var file in response.Files)
+    //        {
+    //            // Get content of file
+    //            var content = file.ApplyTransforms();
+    //            // Remove newlines and replace ' with \\'
+    //            content = content.Replace("'", "\\'").Replace("\r\n", "");
+    //            // Find templateUrl by getting file path and removing inital ~
+    //            var templateUrl = file.IncludedVirtualPath.Replace("~", "").Replace('\\', '/');
+    //            // Add content of template file inside an Angular put method
+    //            strBundleResponse.AppendFormat("t.put('{0}','{1}');", templateUrl, content);
+    //        }
+    //        strBundleResponse.Append(@"}]);");
 
-            // response.Files = new FileInfo[] { };
-            response.Content = strBundleResponse.ToString();
-            response.ContentType = "text/javascript";
-        }
-    }
+    //        // response.Files = new FileInfo[] { };
+    //        response.Content = strBundleResponse.ToString();
+    //        response.ContentType = "text/javascript";
+    //    }
+    //}
 
     public class PartialsBundle : Bundle
     {
-        public PartialsBundle(string moduleName, string virtualPath)
-            : base(virtualPath, new[] { new PartialsTransform(moduleName) })
-        {
-        }
+        //public PartialsBundle(string moduleName, string virtualPath)
+        //    : base(virtualPath, new[] { new PartialsTransform(moduleName) })
+        //{
+        //}
     }
 
     public class BundleConfig
